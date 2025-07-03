e25cd2ad1a9f21e27d75c136ebcd85d92a7e7c1a
Md. Moniruzzaman <mdmoniruzzamanbillal@gmail.com>
2025-07-03 20:31:01 +0600

    client innitialize

---
+++ client/app/index.tsx
@@ -0,0 +1,12 @@
+import { View } from "react-native";
+import { Text } from "react-native-paper";
+
+export default function Index() {
+  return (
+    <View>
+      <Text>Edit app/index.tsx to edit this screen.</Text>
+      <Text>Adding new text , for testing !!!</Text>
+      <Text>Adding new text , for testing !!!</Text>
+    </View>
+  );
+}
