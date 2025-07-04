a380cf80b9999fff03860990cfcf3a1f71bf51f7
Md. Moniruzzaman <mdmoniruzzamanbillal@gmail.com>
2025-07-04 18:06:52 +0600

    image component

--- client/app/index.tsx
+++ client/app/index.tsx
@@ -1,12 +1,18 @@
-import { View } from "react-native";
+import { Image, ScrollView, View } from "react-native";
 import { Text } from "react-native-paper";
 
 export default function Index() {
   return (
-    <View>
-      <Text>Edit app/index.tsx to edit this screen.</Text>
-      <Text>Adding new text , for testing !!!</Text>
-      <Text>Adding new text , for testing !!!</Text>
-    </View>
+    <ScrollView showsVerticalScrollIndicator={false}>
+      <View>
+        <Text>Edit app/index.tsx to edit this screen.</Text>
+        <Text>Adding new text , for testing !!!</Text>
+        <Text>Adding new text , for testing !!!</Text>
+        <Image
+          source={require("@/assets/images/revenue-i1.png")}
+          style={{ height: 100, width: 100 }}
+        />
+      </View>
+    </ScrollView>
   );
 }
